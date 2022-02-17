805a0203e06f416697f097b2dd0ae7137e73b117
alamgir5253 <alamgir5253@gmail.com>
2022-02-17 19:44:31 +0600

    basic style done

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 function getInputValue(costAmount){
     const cost = document.getElementById(costAmount)
 const costValue = parseFloat(cost.value)
-    cost.value = ""
+    // cost.value = ""
 return costValue
 }
 
@@ -19,7 +19,7 @@ const clothAmount = getInputValue("clothes-amount")
 console.log(clothAmount)
 // total cost amount 
 if (foodAmount <= 0 ||rentAmount <= 0 || clothAmount <= 0 ||isNaN(foodAmount) || isNaN(rentAmount) ||isNaN(clothAmount)){
-    alert("enter valid number")
+    document.getElementById("show-error").style.display = "block"
 }
 else if(foodAmount > 0 &&rentAmount > 0 && clothAmount > 0){
     const totalCost =  foodAmount + rentAmount + clothAmount
@@ -27,9 +27,11 @@ else if(foodAmount > 0 &&rentAmount > 0 && clothAmount > 0){
     const totalexpenses = document.getElementById("total-expenses-amount").innerText = totalCost
     const incomeAmount = getInputValue("income-amount")
     const remainAmount = incomeAmount - totalexpenses
+    document.getElementById("show-error").style.display = "none"
     // condition apply to display error massange
     if(incomeAmount < totalexpenses){
         document.getElementById("error-massage").style.display = "block"
+        document.getElementById("balance-amount").innerText = "not enough balance"
     }else{ document.getElementById("balance-amount").innerText = remainAmount
         document.getElementById("error-massage").style.display = "none"
 }
@@ -37,3 +39,41 @@ else if(foodAmount > 0 &&rentAmount > 0 && clothAmount > 0){
 }
 
 })
+
+
+// bonus part start 
+document.getElementById("calculate-saving-amount").addEventListener('click',function(){
+
+    const incomeAmount = getInputValue("income-amount")
+    const savingAmount = getInputValue("saving-input")
+    
+    if (savingAmount < 50){
+        const saving = (incomeAmount / 100)* savingAmount
+        const savingTotal = document.getElementById("saving-amount").innerText = saving
+    }else{
+        const savingTotal = document.getElementById("saving-amount").innerText = "Sorry! You can't save more then 50%"
+    }
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// error massage 
+function errorMassage(){
+    document.getElementById("show-error").style.display ="none"
+}

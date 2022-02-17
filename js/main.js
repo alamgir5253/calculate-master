e7006a259363170974a6158f51625488162b027f
alamgir5253 <alamgir5253@gmail.com>
2022-02-17 22:14:47 +0600

    project done

--- js/main.js
+++ js/main.js
@@ -10,20 +10,19 @@ return costValue
 document.getElementById("calculate-amount").addEventListener('click', function(){
     // food amount calculate
     const foodAmount = getInputValue("food-amount")
-    console.log(foodAmount)
 // rent amount calculate
 const rentAmount = getInputValue("rent-amount")
-console.log(rentAmount)
+
 // cloth amount calculate
 const clothAmount = getInputValue("clothes-amount")
-console.log(clothAmount)
-// total cost amount 
+
+// condition apply to chack negative value and string 
 if (foodAmount <= 0 ||rentAmount <= 0 || clothAmount <= 0 ||isNaN(foodAmount) || isNaN(rentAmount) ||isNaN(clothAmount)){
     document.getElementById("show-error").style.display = "block"
 }
 else if(foodAmount > 0 &&rentAmount > 0 && clothAmount > 0){
     const totalCost =  foodAmount + rentAmount + clothAmount
-    console.log(totalCost)
+
     const totalexpenses = document.getElementById("total-expenses-amount").innerText = totalCost
     const incomeAmount = getInputValue("income-amount")
     const remainAmount = incomeAmount - totalexpenses
@@ -32,38 +31,31 @@ else if(foodAmount > 0 &&rentAmount > 0 && clothAmount > 0){
     if(incomeAmount < totalexpenses){
         document.getElementById("error-massage").style.display = "block"
         document.getElementById("balance-amount").innerText = "not enough balance"
+
     }else{ document.getElementById("balance-amount").innerText = remainAmount
-        document.getElementById("error-massage").style.display = "none"
+        document.getElementById("error-massage").style.display = "none"  
 }
-   
 }
-
 })
-
-
 // bonus part start 
 document.getElementById("calculate-saving-amount").addEventListener('click',function(){
-
     const incomeAmount = getInputValue("income-amount")
     const savingAmount = getInputValue("saving-input")
-    
-    if (savingAmount < 50){
-        const saving = (incomeAmount / 100)* savingAmount
-        const savingTotal = document.getElementById("saving-amount").innerText = saving
+    const remainAmount =document.getElementById("balance-amount")
+    const remainValue = parseFloat(remainAmount.innerText)
+    const saving = (incomeAmount / 100)* savingAmount
+    const finalAmount = remainValue - saving
+    console.log(saving)
+    if (saving < remainValue){
+        // const ssss = remainValue - saving
+         document.getElementById("saving-amount").innerText = saving
+        
+         document.getElementById("remaining-amount").innerText = finalAmount
     }else{
-        const savingTotal = document.getElementById("saving-amount").innerText = "Sorry! You can't save more then 50%"
+        document.getElementById("saving-amount").innerText = "Sorry! You can't save more then what you  have"
+        document.getElementById("remaining-amount").innerText = ""
     }
-
 })
-
-
-
-
-
-
-
-
-
 
 
 
